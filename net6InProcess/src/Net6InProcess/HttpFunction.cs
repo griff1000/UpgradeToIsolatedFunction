d1430941f893aa6cd58df2350cddbff72d3344e7
Ian Griffiths <Ian.Griffiths@confused.com>
2023-06-26 18:02:13 +0100

    All working

--- net6InProcess/src/Net6InProcess/HttpFunction.cs
+++ net6InProcess/src/Net6InProcess/HttpFunction.cs
@@ -1,6 +1,5 @@
 namespace Net6InProcess
 {
-    using FunctionCommon;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Azure.WebJobs;
@@ -9,33 +8,46 @@ namespace Net6InProcess
     using Microsoft.Extensions.Logging;
     using Microsoft.OpenApi.Models;
     using System.Net;
+    using FunctionCommon.Models;
+    using FunctionCommon.Services;
 
-    public static class HttpFunction
+    public class HttpFunction
     {
+        private readonly IMyService _myService;
+
+        public HttpFunction(IMyService myService)
+        {
+            _myService = myService;
+        }
+
         [FunctionName("PostFunction")]
+        #region swagger
         [OpenApiOperation(operationId: "Post", tags: new[] { "name" })]
         [OpenApiRequestBody("application/json", typeof(MyModel), Required = true)]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
-        public static IActionResult Post(
+        #endregion
+        public IActionResult Post(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] MyModel model, ILogger log)
         {
-            var responseMessage = $"Hello, {model.Name}. This HTTP triggered function executed successfully.";
+            var responseMessage = $"{_myService.SayHello(model.Name)}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
         }
 
         [FunctionName("GetFunction")]
+        #region swagger
         [OpenApiOperation(operationId: "Get", tags: new[] { "name" })]
         [OpenApiParameter(name: "name", In = ParameterLocation.Path, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
-        public static IActionResult Get(
+        #endregion
+        public IActionResult Get(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "GetFunction/{name:alpha}")] HttpRequest req, string name, ILogger logger)
         {
             //var name = req.Query["name"].ToString();
 
             var responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"{_myService.SayHello(name)}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
         }
--- net7Isolated/src/Net7Isolated/Program.cs
+++ net7Isolated/src/Net7Isolated/Program.cs
@@ -1,7 +1,17 @@
+using FunctionCommon.Services;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(ConfigureServices)
     .Build();
 
 host.Run();
+
+void ConfigureServices(HostBuilderContext context, IServiceCollection collection)
+{
+    collection.AddSingleton<IMyService, MyService>();
+}
+
+
